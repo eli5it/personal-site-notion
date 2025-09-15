ff2a4299a1e0680b1399ca7a2c602bbb172cf343
eli5it <eli.davis1104@gmail.com>
2025-09-15 17:42:49 -0400

    implemented projects page

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -1,6 +1,7 @@
 import type { Project } from "../lib/types";
 import type { TechnologyColors } from "../lib/types";
 import Image from "next/image";
+import { Link as LinkIcon } from "lucide-react";
 
 const technologyColors: TechnologyColors = {
   React: ["react", "react"],
@@ -37,12 +38,38 @@ function ProjectCard({ project }: ProjectCardProps) {
           <li
             className="border px-2 py-1 rounded"
             key={tech}
-            style={{ borderColor: `var(--color-${technologyColors[tech][0]})` }}
+            style={{
+              borderColor: `var(--color-${technologyColors[tech][0]})`,
+              color: `var(--color-${technologyColors[tech][0]})`,
+            }}
           >
             {tech}
           </li>
         ))}
       </ul>
+
+      <p className="text-dark-border dark:text-offwhite">{description}</p>
+      <div className="flex gap-2">
+        <a
+          className="flex gap-2 border border-black dark:border-dark-border dark:text-offwhite px-1.5 py-1 rounded"
+          href={liveLink}
+        >
+          <LinkIcon className="w-6 h-6" />
+          View Live
+        </a>
+        <a
+          className="flex gap-2 border border-black dark:border-dark-border dark:text-offwhite  px-1.5 py-1 rounded"
+          href={repoLink}
+        >
+          <Image
+            src="/github-icon.svg"
+            alt="github icon"
+            width={24}
+            height={24}
+          ></Image>
+          View Github
+        </a>
+      </div>
     </div>
   );
 }
